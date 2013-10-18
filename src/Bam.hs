6a2119546d061776ab4d21161cadb90a2f51d1d6
Ketil Malde <ketil@malde.org>
2013-10-18 14:53:39 +0200

    whitespace removed
    Ignore-this: 977c0a21884d0a6e7b1837f06e154053
    
    darcs-hash:dbfbca292c1e268df01a0b513f6f9175a6025c36

--- src/Bam.hs
+++ src/Bam.hs
@@ -28,12 +28,12 @@ quants = Quants {
               numrd = Nothing &= help "max number of reads to include"
 --            , bins  =  200    &= help "number of bins" &= typ "INT"
             , mindist = 100   &= help "minimum insert size to count" &= typ "INT"
-            , delta   = 1.01  &= help "relative size of next bin" &= typ "FLOAT"                        
-            , inputs = []     &= args &= typ "BAM file(s)"                
+            , delta   = 1.01  &= help "relative size of next bin" &= typ "FLOAT"
+            , inputs = []     &= args &= typ "BAM file(s)"
             } &= help "output approximate quantiles instead of the full histogram"
-dump = Dump { numrd = Just 100 &= help "max number of reads (default: 100)" 
-            , inputs = []     &= args &= typ "BAM file(s)"            
-            } &= help "Dump alignments in the different classes."       
+dump = Dump { numrd = Just 100 &= help "max number of reads (default: 100)"
+            , inputs = []     &= args &= typ "BAM file(s)"
+            } &= help "Dump alignments in the different classes."
 
 histgen :: Options -> Hist
 histgen o = H 0 [(b,0) | b <- enumFromThenTo size (2*size) (maxdist o)]
@@ -44,8 +44,8 @@ quantgen o = H 0 [(round b,0) | b <- scanl1 (+) $ iterate (* delta o) (fromInteg
 
 main :: IO ()
 main = do
-  o <- cmdArgs $ modes [clfy,hst,quants,dump] 
-       &= help "Extract information from BAM files" 
+  o <- cmdArgs $ modes [clfy,hst,quants,dump]
+       &= help "Extract information from BAM files"
        &= program "bam" &= summary "bam v0.3, ©2012 Ketil Malde"
   when (null $ inputs o) $ error "No input files specified"
   let geninp f = do {putStrLn ("## Input file: "++f) ; (case numrd o of Just x -> take x; Nothing -> id) `fmap` readBams f}
